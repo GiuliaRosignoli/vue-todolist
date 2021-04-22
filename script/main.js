4fa36fb3c0faca607537e0584831d94850939c07
GiuliaRosignoli <giuliarosignoli@gmail.com>
2021-04-22 16:16:52 +0200

    .

--- script/main.js
+++ script/main.js
@@ -26,7 +26,25 @@ const app = new Vue({
     },
     methods: {
         addTodo(){
-        console.log("Add new item here");
+     //   console.log("Add new item here");
+     if(this.newTodo !== "") {
+         this.newtodos.push({
+             text: this.newTodo,
+             completed: false,
+         });
+
+         this.newTodo = "",
+         this.$refs.todoInput.focus();
+     }
+        },
+
+        // Remove the to-do item from the list 
+
+        removeTodo(index){
+            this.todos.splice(index, 1);
+
+            const deleted = this.todos. splice(index, 1)[0];
+            console.log(deleted);
         }
     }
 
